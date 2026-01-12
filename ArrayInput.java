4f8bce5f172a80d9d72d304d80e3e8ecd32860ac
Priyanshu Gupta <priyanshuguptaofficial555@gmail.com>
2026-01-12 13:10:16 +0000

    Add basic Java array

---
+++ ArrayInput.java
@@ -0,0 +1,11 @@
+
+public class ArrayInput {
+  public static void main(String[] args) {
+    
+    String [] foods = {};
+
+    foods[0] = "pizza";
+    
+    System.out.println(foods.length);
+    }
+  }
