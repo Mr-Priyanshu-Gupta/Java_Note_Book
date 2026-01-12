491f793f25584d1281390663836bc40d65e73747
Priyanshu Gupta <priyanshuguptaofficial555@gmail.com>
2026-01-12 13:43:57 +0000

    Add basic Java arrayinput2

---
+++ ArrayInput2.java
@@ -0,0 +1,15 @@
+
+public class ArrayInput2 {
+  public static void main(String[] args) {
+    
+    String [] foods = new String[3];
+
+    foods[0] = "pizza";
+    foods[1] = "Burger";
+    foods[2] = "Chowmen";
+    
+    for(String food : foods){
+    System.out.println(food);
+    }
+  }
+}
