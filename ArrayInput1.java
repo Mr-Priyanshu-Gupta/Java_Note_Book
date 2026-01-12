e321917821e92cb8e27c4b0a8b649d29c713c0d4
Priyanshu Gupta <priyanshuguptaofficial555@gmail.com>
2026-01-12 13:40:07 +0000

    Add basic Java arrayinput1

---
+++ ArrayInput1.java
@@ -0,0 +1,11 @@
+
+public class ArrayInput1 {
+  public static void main(String[] args) {
+    
+    String [] foods = new String[3];
+
+    foods[0] = "pizza";
+    
+    System.out.println(foods.length);
+    }
+  }
