c99b51f0ddc11aaa081558136adb0125cd687736
Priyanshu Gupta <priyanshuguptaofficial555@gmail.com>
2026-01-13 18:31:54 +0000

    Add basic Java arraysearch4

---
+++ ArraySearch4.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+public class ArraySearch4 {
+  public static void main(String[] args) {
+
+   String[] fruits = {"Apple","Orange","Coconut","Banana"};
+   Scanner sc = new Scanner(System.in);
+
+   System.out.print("Enter Fruit Name : ");
+   String target = sc.nextLine();
+
+   boolean isFound = false;
+   
+   int index = 0;
+    for(String fruit : fruits){
+      if(fruit.equals(target)){
+        System.out.println("fruit is found :" + fruit);
+        System.out.println("Fruit found at index: " + index);
+         isFound = true;
+         break;
+      }
+          index++;
+
+     }   
+    
+    if(!isFound){
+    System.out.println("Element Not Found");
+   }  
+  }
+}
