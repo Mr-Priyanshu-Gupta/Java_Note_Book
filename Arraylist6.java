bb678755b7c75e8dad3c8464085107ed5b67db7e
Priyanshu Gupta <priyanshuguptaofficial555@gmail.com>
2026-01-15 18:10:14 +0000

    Add basic Java Arraylist6

---
+++ Arraylist6.java
@@ -0,0 +1,15 @@
+import java.util.Collections;
+import java.util.ArrayList;
+public class Arraylist6 {
+  public static void main(String[] args) {
+
+    ArrayList<String> fruits = new ArrayList<String>();
+
+    fruits.add("Mr.");
+    fruits.add("Priyanshu");
+    fruits.add("Gupta");
+
+   
+    Collections.sort(fruits);
+    System.out.println(fruits);
+  }}
