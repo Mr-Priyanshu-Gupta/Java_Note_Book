e8b17eaf3b78086ee6f2dd00d8ea6f91c7bc78f2
Priyanshu Gupta <priyanshuguptaofficial555@gmail.com>
2026-01-15 18:08:09 +0000

    Add basic Java Arraylist1

---
+++ Arraylist1.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+public class Arraylist1 {
+  public static void main(String[] args) {
+
+    ArrayList<Double> list = new ArrayList<Double>();
+
+    list.add(1.23);
+    list.add(2.45);
+    list.add(3.67);
+
+    System.out.println(list);
+  }}
