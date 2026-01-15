1d675c0468cd3c4d694ac2b01c2dce42c07c63b8
Priyanshu Gupta <priyanshuguptaofficial555@gmail.com>
2026-01-15 18:09:54 +0000

    Add basic Java Arraylist5

---
+++ Arraylist5.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+public class Arraylist5 {
+  public static void main(String[] args) {
+
+    ArrayList<String> fruits = new ArrayList<String>();
+
+    fruits.add("Mr.");
+    fruits.add("Priyanshu");
+    fruits.add("Gupta");
+
+   
+    System.out.println(fruits.get(1));
+    System.out.println(fruits.size());
+    System.out.println(fruits.isEmpty());
+    System.out.println(fruits.contains("Priyanshu"));
+  }}
