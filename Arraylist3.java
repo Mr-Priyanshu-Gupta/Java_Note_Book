ec47a523f1eb969e4a1f0f8fec5e42e78f492afa
Priyanshu Gupta <priyanshuguptaofficial555@gmail.com>
2026-01-15 18:09:13 +0000

    Add basic Java Arraylist3

---
+++ Arraylist3.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+public class Arraylist3 {
+  public static void main(String[] args) {
+
+    ArrayList<String> fruits = new ArrayList<String>();
+
+    fruits.add("Mr.");
+    fruits.add("Priyanshu");
+    fruits.add("Gupta");
+
+    fruits.remove(1);
+    System.out.println(fruits);
+  }}
